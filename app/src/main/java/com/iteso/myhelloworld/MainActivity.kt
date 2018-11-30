f871bf91282145f1beb505aed4938688886381f3
ppgmz <ie697875@iteso.mx>
2018-11-30 08:33:01 -0600

    Added Toast

--- app/src/main/java/com/iteso/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/iteso/myhelloworld/MainActivity.kt
@@ -2,11 +2,14 @@ package com.iteso.myhelloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "Hola bb", Toast.LENGTH_LONG)
     }
 }
